0d347492ac61e4f8c6a1a04d867fb0a993a2ce7b
Mayank <mayank.kumar1_cs23@gla.ac.in>
2025-07-06 21:14:03 +0530

    String

---
+++ 01_basics/04_string.js
@@ -0,0 +1,14 @@
+const name="Mayank"
+const repoCount=50
+
+console.log(name  +  repoCount + " Value ") // Print
+console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`) // Print
+
+const GameName=new String("University") // initialize string
+
+console.log(GameName.length) // Shows string's Length
+console.log(GameName[0]) // It also shows value of particular indeces
+console.log(GameName.charAt(3)) // shows value of index 3
+console.log(GameName.indexOf('v')) // shows at which index 'v' is located
+console.log(GameName.toUpperCase()) // convert in Uppercase
+console.log(GameName.toLowerCase()) // convert in Lowercase
